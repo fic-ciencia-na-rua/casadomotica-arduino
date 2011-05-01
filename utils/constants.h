698f2d961008fe55ebd77c9171020ef2bcf786a6
Santiago Saavedra <santiagosaavedra@gmail.com>
2011-05-01 20:37:15 +0200

    Añadida constante para el programa de IRLED

--- utils/constants.h
+++ utils/constants.h
@@ -5,6 +5,9 @@
 #define DAEMON_VERSION "0.1-dev"
 #define ARDUINO_VERSION "0.1-dev"
 
+/************ Definimos códigos de programa **********/
+
+#define PROGRAM_IRLED 0x01
 
 /************ Definimos bytes de control *************/
 
